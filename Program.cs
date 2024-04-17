436f8e5eb2d6fee67306472e300454dc777e9798
Яковлев Иван Радикович <gr621_yakira@ttit.local>
2024-04-17 17:20:54 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                CalculateRectangleArea();
+                break;
+            case 2:
+                Sum();
+                break;
+            case 3:
+                CheckEven();
+                break;
+            case 4:
+                GenerateRandomNumber();
+                break;
+            default:
+                Console.WriteLine("Некорректный выбор");
+                break;
+        }
+    }
+}
